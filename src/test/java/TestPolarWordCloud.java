42cb375b50ecd09d6a41f8dfdea49b0e0bd3bac6
Kenny Cason <kenneth.cason@gmail.com>
2014-07-03 12:05:32 -0500

    update test/images

--- src/test/java/TestPolarWordCloud.java
+++ src/test/java/TestPolarWordCloud.java
@@ -7,6 +7,7 @@ import wordcloud.PolarWordCloud;
 import wordcloud.WordFrequency;
 import wordcloud.bg.CircleBackground;
 import wordcloud.bg.PixelBoundryBackground;
+import wordcloud.bg.RectangleBackground;
 import wordcloud.font.LinearFontScalar;
 import wordcloud.nlp.FrequencyAnalizer;
 
@@ -64,6 +65,26 @@ public class TestPolarWordCloud {
         wordCloud.build(wordFrequencies, wordFrequencies2);
         LOGGER.info("Took " + (System.currentTimeMillis() - startTime) + "ms to build");
         wordCloud.writeToFile("output/polar_newyork_circle_blur.png");
+    }
+
+    @Test
+    public void newyorkPolarRectangle() throws IOException {
+        final FrequencyAnalizer frequencyAnalizer = new FrequencyAnalizer();
+        frequencyAnalizer.setWordFrequencesToReturn(750);
+        frequencyAnalizer.setMinWordLength(4);
+        frequencyAnalizer.setStopWords(loadStopWords());
+
+        final List<WordFrequency> wordFrequencies = frequencyAnalizer.load(getInputStream("text/new_york_positive.txt"));
+        final List<WordFrequency> wordFrequencies2 = frequencyAnalizer.load(getInputStream("text/new_york_negative.txt"));
+
+        final PolarWordCloud wordCloud = new PolarWordCloud(800, 600, CollisionMode.PIXEL_PERFECT, PolarBlendMode.BLUR);
+        wordCloud.setPadding(2);
+        wordCloud.setBackground(new RectangleBackground(800, 600));
+        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+        final long startTime = System.currentTimeMillis();
+        wordCloud.build(wordFrequencies, wordFrequencies2);
+        LOGGER.info("Took " + (System.currentTimeMillis() - startTime) + "ms to build");
+        wordCloud.writeToFile("output/polar_newyork_rectangle_blur.png");
     }
 
     private static Set<String> loadStopWords() {
