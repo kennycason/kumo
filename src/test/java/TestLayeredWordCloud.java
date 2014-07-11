086982bb93dddac2e31c2f7f13148e7ebadcd25b
Kenny Cason <kenneth.cason@gmail.com>
2014-07-11 03:37:36 -0500

    update pho bowl to 4 layers

--- src/test/java/TestLayeredWordCloud.java
+++ src/test/java/TestLayeredWordCloud.java
@@ -109,34 +109,41 @@ public class TestLayeredWordCloud {
         final List<WordFrequency> wordFrequencies = frequencyAnalizer.load(getInputStream("text/pho_history.txt"));
         final List<WordFrequency> wordFrequencies2 = frequencyAnalizer.load(getInputStream("text/pho_history_viet.txt"));
         final List<WordFrequency> wordFrequencies3 = frequencyAnalizer.load(getInputStream("text/pho_recipee.txt"));
+        final List<WordFrequency> wordFrequencies4 = frequencyAnalizer.load(getInputStream("text/pho_chopsticks.txt"));
 
-        final LayeredWordCloud layeredWordCloud = new LayeredWordCloud(3, 1000, 976, CollisionMode.PIXEL_PERFECT);
-        layeredWordCloud.setBackgroundColor(new Color(0x444444));
+        final LayeredWordCloud layeredWordCloud = new LayeredWordCloud(4, 1000, 976, CollisionMode.PIXEL_PERFECT);
+        layeredWordCloud.setBackgroundColor(new Color(0x333333));
 
         layeredWordCloud.setPadding(0, 1);
         layeredWordCloud.setPadding(1, 1);
         layeredWordCloud.setPadding(2, 1);
+        layeredWordCloud.setPadding(3, 1);
 
         layeredWordCloud.setFontOptions(0, new CloudFont("Comic Sans MS", FontWeight.PLAIN));
         layeredWordCloud.setFontOptions(1, new CloudFont("Comic Sans MS", FontWeight.BOLD));
         layeredWordCloud.setFontOptions(2, new CloudFont("Comic Sans MS", FontWeight.ITALIC));
+        layeredWordCloud.setFontOptions(3, new CloudFont("Comic Sans MS", FontWeight.BOLD));
 
         layeredWordCloud.setBackground(0, new PixelBoundryBackground(getInputStream("backgrounds/pho_1.bmp")));
         layeredWordCloud.setBackground(1, new PixelBoundryBackground(getInputStream("backgrounds/pho_2.bmp")));
         layeredWordCloud.setBackground(2, new PixelBoundryBackground(getInputStream("backgrounds/pho_3.bmp")));
+        layeredWordCloud.setBackground(3, new PixelBoundryBackground(getInputStream("backgrounds/pho_4.bmp")));
 
         layeredWordCloud.setColorPalette(0, new ColorPalette(new Color(0x26A621), new Color(0x21961D), new Color(0x187A15)));
         layeredWordCloud.setColorPalette(1, new ColorPalette(new Color(0x5963F0), new Color(0x515CF0), new Color(0x729FED)));
         layeredWordCloud.setColorPalette(2, new ColorPalette(new Color(0xEDC672), new Color(0xDBB258), new Color(0xDE7C1B)));
+        layeredWordCloud.setColorPalette(3, new ColorPalette(new Color(0x70572B), new Color(0x857150), new Color(0xB09971)));
 
         layeredWordCloud.setFontScalar(0, new SqrtFontScalar(8, 30));
         layeredWordCloud.setFontScalar(1, new SqrtFontScalar(8, 40));
         layeredWordCloud.setFontScalar(2, new SqrtFontScalar(8, 30));
+        layeredWordCloud.setFontScalar(3, new SqrtFontScalar(8, 30));
 
         final long startTime = System.currentTimeMillis();
         layeredWordCloud.build(0, wordFrequencies);
         layeredWordCloud.build(1, wordFrequencies2);
         layeredWordCloud.build(2, wordFrequencies3);
+        layeredWordCloud.build(3, wordFrequencies4);
 
         LOGGER.info("Took " + (System.currentTimeMillis() - startTime) + "ms to build");
         layeredWordCloud.writeToFile("output/layered_pho_bowl.png");
