2a233609558bffac8e7ac137e5d9252fb9b503c6
Wolf <wolf@posdorfer.de>
2015-11-24 19:12:19 +0100

    adding comments adding option to perform non-blocking write call

--- src/main/java/wordcloud/ParallelLayeredWordCloud.java
+++ src/main/java/wordcloud/ParallelLayeredWordCloud.java
@@ -26,10 +26,19 @@ public class ParallelLayeredWordCloud extends LayeredWordCloud {
 
     public ParallelLayeredWordCloud(int layers, int width, int height, CollisionMode collisionMode) {
         super(layers, width, height, collisionMode);
-
         executorservice = Executors.newFixedThreadPool(layers);
     }
 
+    /**
+     * constructs the wordcloud specified by layer using the given
+     * wordfrequencies.<br>
+     * This is a non-blocking call.
+     * 
+     * @param layer
+     *            Wordcloud Layer
+     * @param wordFrequencies
+     *            the WordFrequencies to use as input
+     */
     @Override
     public void build(final int layer, final List<WordFrequency> wordFrequencies) {
         Future<?> f = executorservice.submit(new Runnable() {
@@ -42,30 +51,53 @@ public class ParallelLayeredWordCloud extends LayeredWordCloud {
         executorFutures.add(f);
     }
 
+    /**
+     * Writes the wordcloud to an imagefile.<br>
+     * This is a blocking call.
+     * 
+     * @param outputFileName
+     *            some file like "test.png"
+     */
     @Override
     public void writeToFile(String outputFileName) {
-        // we're not shutting down the executor as this would render it
-        // non-functional afterwards. this way it can still be used
-        LOGGER.info("Awaiting Termination of Executors");
-        for (int i = 0; i < executorFutures.size(); i++) {
-            Future<?> f = executorFutures.get(i);
-            try {
-                // cycle through all futures, invoking get() will block current
-                // task until the future can return a result
-                LOGGER.info("Performing get on Future:" + (i + 1) + "/" + executorFutures.size());
-                f.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+        writeToFile(outputFileName, true);
+    }
+
+    /**
+     * Writes the wordcloud to an imagefile.
+     * 
+     * @param outputFileName
+     *            some file like "test.png"
+     * @param blockThread
+     *            should the current thread be blocked
+     */
+    public void writeToFile(String outputFileName, boolean blockThread) {
+        if (blockThread) {
+            // we're not shutting down the executor as this would render it
+            // non-functional afterwards. this way it can still be used if we
+            // plan on building another layer on top of the previous ones
+            LOGGER.info("Awaiting Termination of Executors");
+            for(int i = 0; i < executorFutures.size(); i++) {
+                Future<?> f = executorFutures.get(i);
+                try {
+                    // cycle through all futures, invoking get() will block
+                    // current task until the future can return a result
+                    LOGGER.info("Performing get on Future:" + (i + 1) + "/" + executorFutures.size());
+                    f.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
             }
+            executorFutures.clear();
+            LOGGER.info("Termination Complete, Processing to File now");
         }
-        executorFutures.clear();
-        LOGGER.info("Termination Complete, Processing to File now");
 
         super.writeToFile(outputFileName);
     }
 
     /**
-     * Signals the ExecutorService to shutdown and await Termination. This is a blocking call.
+     * Signals the ExecutorService to shutdown and await Termination.<br>
+     * This is a blocking call.
      */
     public void shutdown() {
         executorservice.shutdown();
