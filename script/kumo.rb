fcd0d52dc2e8b5881c19daa8412ec729a4b62aff
Kenny Cason <kenneth.cason@gmail.com>
2017-06-02 15:11:24 -0700

    version bump

--- script/kumo.rb
+++ script/kumo.rb
@@ -1,13 +1,13 @@
 class Kumo < Formula
   desc "Kumo: Word Clouds in Java"
   homepage "https://github.com/kennycason/kumo"
-  url "http://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.9/kumo-cli-1.9.jar"
-  sha256 "992273156c9c94454fcd779c521686eeaabb1ed6fdbe048621976b567867fcf2"
+  url "http://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.10/kumo-cli-1.10.jar"
+  sha256 "3a0399c0937053813cd0cefa7437acbeed460fb18ba20ed5b5483ee591f34295"
 
   def install
-    libexec.install "kumo-cli-1.9.jar"
-    bin.write_jar_script libexec/"kumo-cli-1.9.jar", "kumo"
-    puts "Finished installing kumo 1.9"
+    libexec.install "kumo-cli-1.10.jar"
+    bin.write_jar_script libexec/"kumo-cli-1.10.jar", "kumo"
+    puts "Finished installing kumo 1.10"
   end
 
   def server_script(server_jar); <<-EOS.undent
