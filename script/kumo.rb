9c58480c98e96ec176f22eec8d89108cb4a9016a
Kenny Cason <kenneth.cason@gmail.com>
2017-12-01 10:31:09 -0800

    update kumo brew install script

--- script/kumo.rb
+++ script/kumo.rb
@@ -2,7 +2,7 @@ class Kumo < Formula
   desc "Kumo: Word Clouds in Java"
   homepage "https://github.com/kennycason/kumo"
   url "http://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.12/kumo-cli-1.12.jar"
-  sha256 "ac538ca2006d5ce261b9495715cc42752679b577116480cc622d47f8ee1007d0"
+  sha256 "9d67d943676be57fe75d47f1832c4e8cb415ee8e4e82a71d4d7d1e7b4effe7c4"
 
   def install
     libexec.install "kumo-cli-1.12.jar"
