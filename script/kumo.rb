17751a76550affb8fd054b8528b5325395a01a1b
Kenny Cason <kenneth.cason@gmail.com>
2017-12-01 11:00:06 -0800

    update kumo brew install script

--- script/kumo.rb
+++ script/kumo.rb
@@ -4,6 +4,8 @@ class Kumo < Formula
   url "https://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.12/kumo-cli-1.12.jar"
   sha256 "9d67d943676be57fe75d47f1832c4e8cb415ee8e4e82a71d4d7d1e7b4effe7c4"
 
+  depends_on :java => "1.8+"
+
   def install
     libexec.install "kumo-cli-1.12.jar"
     bin.write_jar_script libexec/"kumo-cli-1.12.jar", "kumo"
