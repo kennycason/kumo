bdfe0f48d129b0d4fcafbcc4d532e8d6c68984f7
Kenny Cason <kenneth.cason@gmail.com>
2018-09-15 12:08:37 +0800

    update kumo brew script + doc

--- script/kumo.rb
+++ script/kumo.rb
@@ -2,7 +2,7 @@ class Kumo < Formula
   desc "Word Clouds in Java"
   homepage "https://github.com/kennycason/kumo"
   url "https://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.17/kumo-cli-1.17.jar"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "17f84d6287eeccf361c9eabe6d7449983c13d777afd161105a324c00adcadd0e"
 
   depends_on :java => "1.8+"
 
@@ -16,4 +16,3 @@ class Kumo < Formula
     pipe_output("#{bin}/kumo -i https://wikipedia.org -o /tmp/wikipedia.png", "Generate simple wordcloud")
   end
 end
-ku
