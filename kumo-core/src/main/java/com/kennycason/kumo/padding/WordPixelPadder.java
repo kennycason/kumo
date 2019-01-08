4e882d9ba7fe796551ace229895e8602cf8955cc
joerg1985 <github@nurfuerspam.de>
2019-01-08 22:07:42 +0100

    Moved the boundary check outside the loop
    It is more efficent to check the boudings outside the loop

--- kumo-core/src/main/java/com/kennycason/kumo/padding/WordPixelPadder.java
+++ kumo-core/src/main/java/com/kennycason/kumo/padding/WordPixelPadder.java
@@ -39,24 +39,21 @@ public class WordPixelPadder implements Padder {
     private boolean shouldPad(final CollisionRaster collisionRaster, final int cx, final int cy, final int padding) {
         if (!collisionRaster.isTransparent(cx, cy)) { return false; }
 
-        for (int y = cy - padding; y <= cy + padding; y++) {
-            for (int x = cx - padding; x <= cx + padding; x++) {
+        int minY = Math.max(cy - padding, 0);
+        int maxY = Math.min(cy + padding, collisionRaster.getDimension().height - 1);
+        
+        int minX = Math.max(cx - padding, 0);
+        int maxX = Math.min(cx + padding, collisionRaster.getDimension().width - 1);
+        
+        for (int y = minY; y <= maxY; y++) {
+            for (int x = minX; x <= maxX; x++) {
                 if (x == cx && y == cy) { continue; }
-                if (inBounds(collisionRaster, x, y)) {
-                    if (!collisionRaster.isTransparent(x, y)) {
-                        return true;
-                    }
+                
+                if (!collisionRaster.isTransparent(x, y)) {
+                    return true;
                 }
             }
         }
         return false;
     }
-
-    private boolean inBounds(final CollisionRaster collisionRaster, final int x, final int y) {
-        return x >= 0
-                && y >= 0
-                && x < collisionRaster.getDimension().width
-                && y < collisionRaster.getDimension().height;
-    }
-
 }
