9c467852f025e433538799802383952518266253
kenny <kenneth.cason@gmail.com>
2020-05-11 17:45:08 -0700

    update kumo release script

--- script/kumo.rb
+++ script/kumo.rb
@@ -2,7 +2,7 @@ class Kumo < Formula
   desc "Word Clouds in Java"
   homepage "https://github.com/kennycason/kumo"
   url "https://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.27/kumo-cli-1.27.jar"
-  sha256 "17f84d6287eeccf361c9eabe6d7449983c13d777afd161105a324c00adcadd0e"
+  sha256 "f3643a734e6f8d9c31b0a50f7d4df3f99ea0bf81377efd5a79291b2fff51f1e6"
 
   depends_on :java => "1.8+"
 
